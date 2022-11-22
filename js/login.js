17539d1ea55b61d3ea6f6b20b34e8d0086f4d404
Xinyi110 <750537172@qq.com>
2022-11-22 19:55:03 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,9 +2,11 @@ function userlogin(){
     var userName = document.getElementById("user");
     var userPassword = document.getElementById("password");
     if (userName.value == 15938788961 && userPassword.value == 971011) {
-        window.location.href="./user/main.html"; //当前页面跳转
+        window.location.href="./html/user/main.html"; //当前页面跳转
         // window.open("../html/user/timer.html");      //子页面跳转
         return false;
-    } 
+    } else{
+        alert("用户名或者密码错误，请重试...");
+    }
     return false;
 }
