a27d328e845756f9bb7042e10b0389a6ad76bb69
Xinyi110 <750537172@qq.com>
2022-11-22 19:42:18 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function jptimer(){
+    // window.location.href="../user/timer.html"; //当前页面跳转
+    document.getElementById("right-box").innerHTML = '<object type="text/html" data="timer.html" width="100%" height="100%"></object>';
+    return false;
+}
+
+function jprotatephoto(){
+    // window.location.href="../user/rotatephoto.html"; //当前页面跳转
+    document.getElementById("right-box").innerHTML = '<object type="text/html" data="rotatephoto.html" width="100%" height="100%"></object>';
+return false;
+}
+
+function other(){
+    alert("正在努力测试中......");
+    // window.location.href="../user/rotatephoto.html"; //当前页面跳转
+    // document.getElementById("right-box").innerHTML = '<object type="text/html" data="rotatephoto.html" width="100%" height="100%"></object>';
+}
